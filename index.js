a39825277b2ddf52983c06dcb05f709c2fe5e75f
David Richards <drichards@twitter.com>
2017-05-04 13:21:33 -0400

    Some changes

--- index.js
+++ index.js
@@ -2,33 +2,31 @@ var express = require('express');
 var app = express();
 var pg = require('pg');
 
-pg.defaults.ssl = true;
-
-pg.connect(process.env.DATABASE_URL, function(err, client) {
-  if (err) throw err;
-  console.log('Connected to postgres! Getting schemas...');
-
-  client
-    .query('SELECT table_schema,table_name FROM information_schema.tables;')
-    .on('row', function(row) {
-      console.log(JSON.stringify(row));
-    });
-});
+//pg.defaults.ssl = true;
 
 app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/public'));
-
 // views is directory for all template files
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
-app.get('/', function(request, response) {
-  response.render('pages/index');
-});
 
-app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
+pg.connect(process.env.DATABASE_URL, function(err, client) {
+  if (err) throw err;
+
+	app.get('/', function(request, response) {
+	  var users = [];
+	  client.query('SELECT username, rank FROM public.users order by rank desc;', function(err, result) {
+	  	response.render('pages/index', { users: result.rows });
+	  });
+	});
+
+	app.listen(app.get('port'), function() {
+	  console.log('Node app is running on port', app.get('port'));
+	});
+
+  
 });
 
 
